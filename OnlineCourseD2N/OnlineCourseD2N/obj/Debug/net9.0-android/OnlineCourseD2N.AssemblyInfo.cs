242098dd85bffbb287a25796d96bbd9e3188f870
Stevanus Denko <stevanusdenko46@gmail.com>
2025-11-23 09:34:56 +0700

    camera

--- OnlineCourseD2N/OnlineCourseD2N/obj/Debug/net9.0-android/OnlineCourseD2N.AssemblyInfo.cs
+++ OnlineCourseD2N/OnlineCourseD2N/obj/Debug/net9.0-android/OnlineCourseD2N.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineCourseD2N")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+7ba18519ba496412fb3b4b3cb279f35beb573b42")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+e22cb7c984b95b2a72445165c85e956fe8bdea2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineCourseD2N")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineCourseD2N")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
